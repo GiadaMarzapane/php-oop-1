f22654fd8b131811c5f47de6fad2fb253a4f86d7
GiadaMarzapane <giada.ortesta@gmail.com>
2023-03-03 15:31:00 +0100

    Prova stampa istanze

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-class Movie{
-    public $tile;
+class Movie {
+    public $title;
     public $director;
     public $duration;
     public $year_of_release;
 }
+
+$reservoir_dogs = new Movie();
+$reservoir_dogs->title = 'Reservoir Dogs';
+$reservoir_dogs->director = 'Quentin Tarantino';
+$reservoir_dogs->duration = '99';
+$reservoir_dogs->year_of_release = '1992';
+var_dump($reservoir_dogs);
+
+
+$pulp_fiction = new Movie();
+$pulp_fiction->title = 'Pulp Fiction';
+$pulp_fiction->director = 'Quentin Tarantino';
+$pulp_fiction->duration = '154';
+$pulp_fiction->year_of_release = '1994';
+var_dump($pulp_fiction);
