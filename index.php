cda3bc0707c68cc283ef2c03de8102f0a48109d4
GiadaMarzapane <giada.ortesta@gmail.com>
2023-03-03 15:57:23 +0100

    Add construct

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ class Movie {
     public $duration;
     private $year_of_release;
 
+    function __construct(string $_title)
+    {
+        $this->title = $_title;
+    }
+
     public function setYear($year_of_release){
         if(is_string($year_of_release)){
             echo 'Errore, inserisci un anno valido';
@@ -18,17 +23,16 @@ class Movie {
     }
 }
 
-$reservoir_dogs = new Movie();
-$reservoir_dogs->title = 'Reservoir Dogs';
+$reservoir_dogs = new Movie('Reservoir Dogs');
 $reservoir_dogs->director = 'Quentin Tarantino';
 $reservoir_dogs->duration = 99;
 $reservoir_dogs->setYear(1992);
 $reservoir_dogs->getYear();
 var_dump($reservoir_dogs);
 
+echo '<br>';
 
-$pulp_fiction = new Movie();
-$pulp_fiction->title = 'Pulp Fiction';
+$pulp_fiction = new Movie('Pulp Fiction');
 $pulp_fiction->director = 'Quentin Tarantino';
 $pulp_fiction->duration = 154;
 $pulp_fiction->setYear(1994);
